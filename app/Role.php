e3d45fd34a4d05e273158233455dd7a12d4b33b4
Jose Palazuelos <punksolid@gmail.com>
2019-02-25 15:43:19 -0700

    tenant más explicito

--- app/Role.php
+++ app/Role.php
@@ -5,17 +5,27 @@ namespace App;
 use Hyn\Tenancy\Models\Website;
 use Hyn\Tenancy\Traits\UsesTenantConnection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Spatie\Permission\Models\Role as SpatieRole;
+use Spatie\Permission\PermissionRegistrar;
 
 class Role extends SpatieRole
 {
     use UsesTenantConnection;
-//
-//    public function syncPermissions(array ...$permissions)
-//    {
-//
-//    }
 
+    /**
+     * A role may be given various permissions.
+     */
+    public function permissions(): BelongsToMany
+    {
+        $database = $this->getConnection()->getDatabaseName();
+        $table_name = config('permission.table_names.role_has_permissions');
+        return $this->belongsToMany(Permission::class, "$database.$table_name");
+//        return $this->belongsToMany(
+//            config('permission.models.permission'),
+//            config('permission.table_names.role_has_permissions')
+//        );
+    }
     /**
      * Remove all current permissions and set the given ones.
      *
@@ -29,7 +39,95 @@ class Role extends SpatieRole
         $this->permissions()->detach();
 
         return $this->givePermissionTo($permissions);
-//        return
     }
 
+//    public function givePermissionTo(array ...$permissions)
+//    {
+//    }
+
+    /**
+     * Grant the given permission(s) to a role.
+     *
+     * @param string|array|\Spatie\Permission\Contracts\Permission|\Illuminate\Support\Collection $permissions
+     *
+     * @return $this
+     */
+    public function givePermissionTo(...$permissions)
+    {
+        $permissions = collect($permissions)
+            ->flatten()
+            ->map(function ($permission) {
+                return $this->getStoredPermission($permission);
+            })
+            ->filter(function ($permission) {
+                return $permission instanceof Permission;
+            })
+            ->each(function ($permission) {
+                $this->ensureModelSharesGuard($permission);
+            })
+            ->map->id
+            ->all();
+
+        $model = $this->getModel();
+
+        if ($model->exists) {
+            $this->permissions()->sync($permissions, false);
+            $model->load('permissions');
+        } else {
+            $class = \get_class($model);
+
+            $class::saved(
+                function ($object) use ($permissions, $model) {
+                    static $modelLastFiredOn;
+                    if ($modelLastFiredOn !== null && $modelLastFiredOn === $model) {
+                        return;
+                    }
+                    $object->permissions()->sync($permissions, false);
+                    $object->load('permissions');
+                    $modelLastFiredOn = $object;
+                }
+            );
+        }
+
+        $this->forgetCachedPermissions();
+
+        return $this;
+    }
+
+    /**
+     * @param string|array|\Spatie\Permission\Contracts\Permission|\Illuminate\Support\Collection $permissions
+     *
+     * @return \Spatie\Permission\Contracts\Permission|\Spatie\Permission\Contracts\Permission[]|\Illuminate\Support\Collection
+     */
+    protected function getStoredPermission($permissions)
+    {
+        $permissionClass = $this->getPermissionClass();
+
+        if (is_numeric($permissions)) {
+            return $permissionClass->findById($permissions, $this->getDefaultGuardName());
+        }
+
+        if (is_string($permissions)) {
+            return $permissionClass->findByName($permissions, $this->getDefaultGuardName());
+        }
+
+        if (is_array($permissions)) {
+            return $permissionClass
+                ->whereIn('name', $permissions)
+                ->whereIn('guard_name', $this->getGuardNames())
+                ->get();
+        }
+
+        return $permissions;
+    }
+
+    public function getPermissionClass()
+    {
+        if (! isset($this->permissionClass)) {
+//            $this->permissionClass = app(PermissionRegistrar::class)->getPermissionClass();
+            $this->permissionClass = new Permission();
+        }
+
+        return $this->permissionClass;
+    }
 }
