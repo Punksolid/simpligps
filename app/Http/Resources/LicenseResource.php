7c676788d6cfc018053d314f28981e88c8a3c594
Jose Palazuelos <punksolid@gmail.com>
2019-02-25 03:03:23 -0700

    agregado id a license

--- app/Http/Resources/LicenseResource.php
+++ app/Http/Resources/LicenseResource.php
@@ -25,6 +25,7 @@ class LicenseResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
             "name" => $this->name,
             "description" => $this->description,
             "lapse" => $this->lapse,
