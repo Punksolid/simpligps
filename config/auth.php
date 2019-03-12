22cc6d1778c88fa4dd1f4e5c152d8ffd9194be0b
Jose Palazuelos <punksolid@gmail.com>
2019-03-12 05:25:36 -0700

    TTL 30 minutos

--- config/auth.php
+++ config/auth.php
@@ -110,7 +110,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 60,
+            'expire' => 120,
         ],
     ],
 
