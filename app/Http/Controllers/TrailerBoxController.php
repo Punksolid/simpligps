ac529d7464b4da4925a35080f02bc56ebee611e6
Jose Palazuelos <punksolid@gmail.com>
2019-04-16 15:04:49 -0600

    Archivos iniciales

---
+++ app/Http/Controllers/TrailerBoxController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TrailerBox;
+use Illuminate\Http\Request;
+
+class TrailerBoxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TrailerBox  $trailerBox
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TrailerBox $trailerBox)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\TrailerBox  $trailerBox
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TrailerBox $trailerBox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TrailerBox  $trailerBox
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TrailerBox $trailerBox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TrailerBox  $trailerBox
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TrailerBox $trailerBox)
+    {
+        //
+    }
+}
---
+++ database/factories/TrailerBoxFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\TrailerBox;
+use Faker\Generator as Faker;
+
+$factory->define(TrailerBox::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
