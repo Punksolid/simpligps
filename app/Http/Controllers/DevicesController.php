cd2428af4e1bc310d29b0bbbeccf4c5f301d7743
Jose Palazuelos <punksolid@gmail.com>
2019-05-04 14:07:03 -0600

    Agregados endpoints de busqueda en operators, trucks, places, y devices

--- app/Http/Controllers/DevicesController.php
+++ app/Http/Controllers/DevicesController.php
@@ -9,13 +9,14 @@ use App\Http\Resources\DeviceResource;
 use App\Point;
 use Illuminate\Http\Request;
 use Punksolid\Wialon\Unit;
+use App\Interfaces\SearchInterface;
 
 /**
  * Class DevicesController
  * @package App\Http\Controllers
  * @resource Device
  */
-class DevicesController extends Controller
+class DevicesController extends Controller implements SearchInterface
 {
 
     /**
@@ -137,5 +138,16 @@ class DevicesController extends Controller
         }
 
         abort(500);
+    }
+
+    public function search(Request $request) 
+    {
+        $devices = Device::query()
+            ->where('name', 'LIKE', "%{$request->name}%")
+            ->get();
+
+        return DeviceResource::collection($devices);
+
+
     }
 }
--- app/Http/Controllers/OperatorsController.php
+++ app/Http/Controllers/OperatorsController.php
@@ -6,13 +6,14 @@ use App\Http\Requests\OperatorRequest;
 use App\Http\Resources\OperatorResource;
 use App\Operator;
 use Illuminate\Http\Request;
+use App\Interfaces\SearchInterface;
 
 /**
  * Class OperatorController
  * @package App\Http\Controllers
  * @resource Operadores
  */
-class OperatorsController extends Controller
+class OperatorsController extends Controller implements SearchInterface
 {
     /**
      * Muestra listado de operadores.
@@ -94,5 +95,15 @@ class OperatorsController extends Controller
         }
 
         return response(["message" => "Ocurrió un error al eliminar el operador."])->setStatusCode(422);
+    }
+
+    public function search(\Illuminate\Http\Request $request)
+    {
+        
+        $operators = Operator::query()
+            ->where('name', 'LIKE', "%{$request->name}%")
+            ->get();
+
+        return OperatorResource::collection($operators);
     }
 }
