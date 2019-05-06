b1afac245887b1f590561ebbb16c287f4d081ad3
Jose Palazuelos <punksolid@gmail.com>
2019-05-05 19:41:38 -0600

    Purchases

---
+++ app/Http/Controllers/PurchaseController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Purchase;
+use Illuminate\Http\Request;
+use OpenApi\Annotations\License;
+use App\License as AppLicense;
+use Illuminate\Support\Str;
+use App\Events\AccountCreatedEvent;
+use App\Http\Resources\AccountResource;
+
+class PurchaseController extends Controller
+{
+
+    /**
+     * Store a newly created account in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return AccountResource
+     */
+    public function storeFromEcommerce(Request $request)
+    {
+        
+        $validatedDataArry = $this->validate($request, [
+            'billing.email' => 'required|email',
+            'line_items.0.sku' => 'required'
+        ]);
+        // dd($validatedDataArry);
+        // // dd($request->line_items[0]["sku"]);
+        // // dd($request->toArray());
+        // // dd($request->billing["email"]);
+
+        // $license = $request->line_items[0]["sku"];
+        $license = AppLicense::where("name", $request->line_items[0]["sku"])->firstOrFail();
+        $email = $request->billing["email"];
+        $purchase = Purchase::create([
+            "email" => $request->billing["email"],
+            "audit" => $request->all()
+        ]);
+        $account = new \App\Account([
+            "easyname" => $request->billing["email"]
+        ]);
+
+        $account->createAccount();
+        $account->addLicense($license);
+        $user = \App\User::firstOrCreate([
+            "email" => $request->billing["email"]
+        ], [
+            'email' => $request->billing["email"],
+            'name' => '',
+            'lastname' => '',
+            'password' => bcrypt(Str::random(10))
+        ]);
+
+        $account->addUser($user);
+
+        event(new AccountCreatedEvent($user, $account));
+
+        return AccountResource::make($account);
+    }
+
+}
---
+++ database/migrations/2019_05_05_044829_create_purchases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email');
+            $table->json("audit");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
