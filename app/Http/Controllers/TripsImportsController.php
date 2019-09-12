7b5734b1cd41c408cd1db25504f4c7fea260bae6
Jose Palazuelos <punksolid@gmail.com>
2019-09-12 04:45:03 -0600

    small validations for import

--- app/Http/Controllers/TripsImportsController.php
+++ app/Http/Controllers/TripsImportsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Imports\TripImport;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Maatwebsite\Excel\Facades\Excel;
 use Maatwebsite\Excel\Validators\ValidationException;
 
@@ -12,13 +13,20 @@ class TripsImportsController extends Controller
 {
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'trips' => 'required|file'
+        ]);
 
         $file = $request->file('trips');
+        
+        Storage::disk('tenant')->put('readme.md', 'Hi John.');
         \DB::connection('tenant')->beginTransaction();
         try {
             Excel::import(
                 new TripImport($file),
-                    $file->getRealPath()
+                    $file->getRealPath(),
+                    null,
+                    \Maatwebsite\Excel\Excel::XLSX
             );
         } catch (\Exception $exception) {
 
