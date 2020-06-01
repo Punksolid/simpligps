62db9fdbb1d2029c2e86f4b8f8b7f3e66e3d1396
Punksolid <punksolid@gmail.com>
2020-05-31 20:26:46 -0500

    default configuration

--- config/traccar.php
+++ config/traccar.php
@@ -3,7 +3,7 @@
 return [
     'base_url' => env('TRACCAR_BASE_URL'),
 
-    'debug_requests' => true,
+    'debug_requests' => env('TRACCAR_DEBUG_REQUEST', env('TELESCOPE_ENABLED', false)),
 
     'auth' => [
         'username' => env('TRACCAR_USERNAME'),
